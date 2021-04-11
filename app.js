4ab3128be058b26291fffc5abbd1f189817e656e
Eri <trgianghuynhdev1808@gmail.com>
2021-04-11 12:34:44 +0700

    imp: volume, time slider UI

--- app.js
+++ app.js
@@ -2,39 +2,75 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 URL = window.URL || window.webkitURL;
 
 const audioTypeSelect = document.getElementById("audioTypeSelect");
-const startRecordBtn = document.getElementById("startBtn");
-const stopRecordBtn = document.getElementById("stopBtn");
-const recordingList = document.getElementById("recordingList");
-const playAudioBtn = document.getElementById("playBtn");
+const startRecordBtn = document.getElementById("start-btn");
+const stopRecordBtn = document.getElementById("stop-btn");
+const recordingList = document.getElementById("recording-list");
+const playAudioBtn = document.getElementById("play-icon");
+const muteAudioBtn = document.getElementById("mute-icon");
+const seekSlider = document.getElementById("seek-slider");
+const volumeSlider = document.getElementById("volume-slider");
+const audioPlayerContainer = document.getElementById("audio-player-container");
+
+let audioContext, input, encodingType, recorder, curStream;
+let playState = "play";
+let muteState = "unmute";
 
 startRecordBtn.addEventListener("click", startRecord);
 stopRecordBtn.addEventListener("click", stopRecord);
 
-let audioContext, input, encodingType, recorder, curStream;
-let state = "play";
-
 // make animation for play audio btn
-const animation = lottie.loadAnimation({
+const playAnimation = lottie.loadAnimation({
   container: playAudioBtn,
   path:
     "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json",
   renderer: "svg",
   loop: false,
   autoplay: false,
-  name: "Demo Audio",
+  name: "Play Animation",
 });
 
-animation.goToAndStop(14, true);
+playAnimation.goToAndStop(14, true);
+
 playAudioBtn.addEventListener("click", () => {
-  if (state === "play") {
-    animation.playSegments([14, 27], true);
-    state = "pause";
+  if (playState === "play") {
+    playAnimation.playSegments([14, 27], true);
+    playState = "pause";
   } else {
-    animation.playSegments([0, 14], true);
-    state = "play";
+    playAnimation.playSegments([0, 14], true);
+    playState = "play";
   }
 });
-//end make animation for paly audio btn
+//end make animation for play audio btn
+
+// make animation for mute audio btn
+const muteAnimation = lottie.loadAnimation({
+  container: muteAudioBtn,
+  path:
+    "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/mute/mute.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  name: "Mute Animation",
+});
+
+muteAudioBtn.addEventListener("click", () => {
+  if (muteState === "unmute") {
+    muteAnimation.playSegments([0, 15], true);
+    muteState = "mute";
+  } else {
+    muteAnimation.playSegments([15, 25], true);
+    muteState = "unmute";
+  }
+});
+// end make animation for mute audio btn
+
+seekSlider.addEventListener("input", (e) => {
+  showRangeProgress(e.target);
+});
+
+volumeSlider.addEventListener("input", (e) => {
+  showRangeProgress(e.target);
+});
 
 function startRecord() {
   const constraints = { audio: true, video: false };
@@ -109,3 +145,17 @@ function createAudio(blob, encoding) {
 
   recordingList.appendChild(liEle);
 }
+
+const showRangeProgress = (rangeInput) => {
+  if (rangeInput === seekSlider) {
+    audioPlayerContainer.style.setProperty(
+      "--seek-before-width",
+      (rangeInput.value / rangeInput.max) * 100 + "%"
+    );
+  } else {
+    audioPlayerContainer.style.setProperty(
+      "--volume-before-width",
+      (rangeInput.value / rangeInput.max) * 100 + "%"
+    );
+  }
+};
