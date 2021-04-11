1d20b776852f2bccbe1f4521324c8e48fdc3e7ee
Eri <trgianghuynhdev1808@gmail.com>
2021-04-11 11:18:47 +0700

    imp: play audio  button animate

--- app.js
+++ app.js
@@ -4,13 +4,37 @@ URL = window.URL || window.webkitURL;
 const audioTypeSelect = document.getElementById("audioTypeSelect");
 const startRecordBtn = document.getElementById("startBtn");
 const stopRecordBtn = document.getElementById("stopBtn");
-const logArea = document.getElementById("log");
 const recordingList = document.getElementById("recordingList");
+const playAudioBtn = document.getElementById("playBtn");
 
 startRecordBtn.addEventListener("click", startRecord);
 stopRecordBtn.addEventListener("click", stopRecord);
 
 let audioContext, input, encodingType, recorder, curStream;
+let state = "play";
+
+// make animation for play audio btn
+const animation = lottie.loadAnimation({
+  container: playAudioBtn,
+  path:
+    "https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json",
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+  name: "Demo Audio",
+});
+
+animation.goToAndStop(14, true);
+playAudioBtn.addEventListener("click", () => {
+  if (state === "play") {
+    animation.playSegments([14, 27], true);
+    state = "pause";
+  } else {
+    animation.playSegments([0, 14], true);
+    state = "play";
+  }
+});
+//end make animation for paly audio btn
 
 function startRecord() {
   const constraints = { audio: true, video: false };
@@ -72,7 +96,7 @@ function createAudio(blob, encoding) {
   const linkEle = document.createElement("a");
 
   //add src & control for audio element
-  audioEle.controls = true;
+  audioEle.controls = false;
   audioEle.src = url;
 
   //create link download from blob
